389b55a0ce1330519155ccd3e878d9d74df31b73
tobias.fritz_bcmsolu <tobias.fritz@bcmsolutions.de>
2022-06-07 17:02:04 +0200

    some refactoring

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,25 +1,13 @@
 <template>
   <div class="board center">
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[0] = $event" :hasMole="hasMole[0]" :active="numberBurrows > 0"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[1] = $event" :hasMole="hasMole[1]" :active="numberBurrows > 1"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[2] = $event" :hasMole="hasMole[2]" :active="numberBurrows > 2"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[3] = $event" :hasMole="hasMole[3]" :active="numberBurrows > 3"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[4] = $event" :hasMole="hasMole[4]" :active="numberBurrows > 4"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[5] = $event" :hasMole="hasMole[5]" :active="numberBurrows > 5"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[6] = $event" :hasMole="hasMole[6]" :active="numberBurrows > 6"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[7] = $event" :hasMole="hasMole[7]" :active="numberBurrows > 7"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[8] = $event" :hasMole="hasMole[8]" :active="numberBurrows > 8"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[9] = $event" :hasMole="hasMole[9]" :active="numberBurrows > 9"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[10] = $event" :hasMole="hasMole[10]" :active="numberBurrows > 10"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[11] = $event" :hasMole="hasMole[11]" :active="numberBurrows > 11"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[12] = $event" :hasMole="hasMole[12]" :active="numberBurrows > 12"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[13] = $event" :hasMole="hasMole[13]" :active="numberBurrows > 13"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[14] = $event" :hasMole="hasMole[14]" :active="numberBurrows > 14"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[15] = $event" :hasMole="hasMole[15]" :active="numberBurrows > 15"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[16] = $event" :hasMole="hasMole[16]" :active="numberBurrows > 16"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[17] = $event" :hasMole="hasMole[17]" :active="numberBurrows > 17"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[18] = $event" :hasMole="hasMole[18]" :active="numberBurrows > 18"/>
-    <MoleBurrow @hit="calcScoreGain" @update:hasMole="hasMole[19] = $event" :hasMole="hasMole[19]" :active="numberBurrows > 19"/>
+    <MoleBurrow
+        v-for="n in hasMole.length"
+        :key="n-1"
+        @hit="calcScoreGain"
+        @update:hasMole="hasMole[n-1] = $event"
+        :hasMole="hasMole[n-1]"
+        :active="numberBurrows > (n-1)"
+    />
   </div>
 </template>
 
@@ -40,13 +28,7 @@ export default {
   },
   emits: ['score-gain'],
   setup (props, { emit }) {
-    const hasMole = ref([
-      false, false, false, false,
-      false, false, false, false,
-      false, false, false, false,
-      false, false, false, false,
-      false, false, false, false,
-    ])
+    const hasMole = ref(Array(props.numberBurrows).fill(false))
     const isGameRunning = ref(false)
     const timer = ref(null)
 
@@ -108,13 +90,7 @@ export default {
      */
     function stopGame () {
       isGameRunning.value = false
-      hasMole.value = [
-        false, false, false, false,
-        false, false, false, false,
-        false, false, false, false,
-        false, false, false, false,
-        false, false, false, false,
-      ]
+      hasMole.value = Array(props.numberBurrows).fill(false)
     }
 
     return {
